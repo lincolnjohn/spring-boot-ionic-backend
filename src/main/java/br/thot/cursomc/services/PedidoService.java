a4994bde7167d78096d930af6a6be683ff3aee35
Lincoln John <lincolnjohn@ufpa.br>
2018-05-10 16:08:09 -0300

    Endpoint/pedidos/{id} disponibilizado

---
+++ src/main/java/br/thot/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package br.thot.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.thot.cursomc.domain.Pedido;
+import br.thot.cursomc.repositories.PedidoRepository;
+import br.thot.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+id+", Tipo: "+Pedido.class.getName()));
+		
+	}
+
+}
