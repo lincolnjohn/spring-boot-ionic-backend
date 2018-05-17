0bed4a8f84122b9d5c64d79585c75f76747057c0
Lincoln John <lincolnjohn@ufpa.br>
2018-05-17 12:09:09 -0300

    Listando todas Categorias com DTO

--- src/main/java/br/thot/cursomc/services/CategoriaService.java
+++ src/main/java/br/thot/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.thot.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,6 +50,11 @@ public class CategoriaService {
 		
 		
 		
+	}
+
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
 	}
 
 }
