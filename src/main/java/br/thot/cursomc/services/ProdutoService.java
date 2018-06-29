836785f18158ad58c3cc38fd991add78485cacb2
Lincoln John <lincolnjohn@ufpa.br>
2018-06-29 17:22:56 -0300

    Aula 47 partir 1 - parcial

---
+++ src/main/java/br/thot/cursomc/services/ProdutoService.java
@@ -0,0 +1,44 @@
+package br.thot.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import br.thot.cursomc.domain.Categoria;
+import br.thot.cursomc.domain.Produto;
+import br.thot.cursomc.repositories.CategoriaRepository;
+import br.thot.cursomc.repositories.ProdutoRepository;
+import br.thot.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		
+		Optional<Produto> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+id+", Tipo: "+Produto.class.getName()));
+		
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids , Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		List<Categoria> categorias = categoriaRepository.findAll(ids); 
+	
+		return repo.search(nome,categorias,pageRequest);
+	}
+
+}
