2c9d16e15a18e81a8bc039e10d505ac2e4dad7c8
Lincoln John <lincolnjohn@ufpa.br>
2018-07-11 12:15:17 -0300

    Busca de produtos por nome e categoria

--- src/main/java/br/thot/cursomc/CursomcApplication.java
+++ src/main/java/br/thot/cursomc/CursomcApplication.java
@@ -117,7 +117,7 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		//produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 		
 		Estado est1 = new Estado(null, "Para");
--- src/main/java/br/thot/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/br/thot/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,25 @@
 package br.thot.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.thot.cursomc.domain.Categoria;
 import br.thot.cursomc.domain.Produto;
 
 @Repository 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+	//@Query("select distinct obj from Produto obj inner join obj.categorias cat where obj.nome like %:nome% and cat in :categorias")
+	//Page<Produto> search(@Param("nome") String nome,@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 
 }
